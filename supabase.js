3f3534546ba4e80d2df7a84b8e5ac86d5cfc6611
kaikoliveira1991-wq <kaikoliveira1991@gmail.com>
2026-09-06 16:49:58 -0300

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://xllpwdrinzsbzexaheiz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_h1BgIb4A20gW1qQjRdQglg_9FhNwmXw";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
